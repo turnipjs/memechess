f818ce804faeef400005d950914bbfd3fe7c8397
mynusplus <33581931+mynusplus@users.noreply.github.com>
2017-11-12 01:52:45 -0600

    Update beekeeper.py

--- datamodel/beekeeper.py
+++ datamodel/beekeeper.py
@@ -5,22 +5,23 @@ class Beekeeper(Piece):
 	def get_actions(self):
 		actions = []
 
-		for vector in ((0, 1, 0), (1, 0, 0), (1, 1, 0), (0, -1, 0), (-1, 0, 0),
+		if can_move == True:
+			for vector in ((0, 1, 0), (1, 0, 0), (1, 1, 0), (0, -1, 0), (-1, 0, 0),
 					   (-1, -1, 0), (1, -1, 0), (-1, 1, 0)):
-			res = self.game.step_move_to(self, self.pos, vector)
-			if res.type == MoveResult.Type.REGULAR:
-				actions.append(Action("move", *res.pos))
-				if res.ends_motion:
+				res = self.game.step_move_to(self, self.pos, vector)
+				if res.type == MoveResult.Type.REGULAR:
+					actions.append(Action("move", *res.pos))
+					if res.ends_motion:
+						continue
+				elif res.type == MoveResult.Type.CAPTURE:
+					actions.append(Action("capture", *res.pos))
+				else:
 					continue
-			elif res.type == MoveResult.Type.CAPTURE:
-				actions.append(Action("capture", *res.pos))
-			else:
-				continue
 
-			step2 = self.game.step_move_to(self, res.pos, vector)
+				step2 = self.game.step_move_to(self, res.pos, vector)
 
-			if step2.type == MoveResult.Type.REGULAR:
-				actions.append(Action("move", *step2.pos))
+				if step2.type == MoveResult.Type.REGULAR:
+					actions.append(Action("move", *step2.pos))
 
 		return actions
 
