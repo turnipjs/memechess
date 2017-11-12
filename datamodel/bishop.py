e65eb19f4fb50a4857efcb7df2d35c971ab78b3a
mynusplus <33581931+mynusplus@users.noreply.github.com>
2017-11-12 01:55:46 -0600

    Update bishop.py

--- datamodel/bishop.py
+++ datamodel/bishop.py
@@ -7,18 +7,19 @@ class Bishop(Pawn):
     def get_actions(self):
         actions = []
         
-        for vector in ((1, 1, 0), (1, -1, 0), (-1, 1, 0), (-1, -1, 0)):
-            res = MoveResult(MoveResult.Type.REGULAR, self.pos)
-            while True:
-                res = self.game.step_move_to(self, res.pos, vector)
-                if res.type == MoveResult.Type.INVALID:
-                    break
-                elif res.type == MoveResult.Type.REGULAR:
-                    actions.append(Action("move", *res.pos))
-                    if res.ends_motion:
+        if can_move == True:
+            for vector in ((1, 1, 0), (1, -1, 0), (-1, 1, 0), (-1, -1, 0)):
+                res = MoveResult(MoveResult.Type.REGULAR, self.pos)
+                while True:
+                    res = self.game.step_move_to(self, res.pos, vector)
+                    if res.type == MoveResult.Type.INVALID:
                         break
-                elif res.type == MoveResult.Type.CAPTURE:
-                    actions.append(Action("move_capture", *res.pos))
-                    break
+                    elif res.type == MoveResult.Type.REGULAR:
+                        actions.append(Action("move", *res.pos))
+                        if res.ends_motion:
+                            break
+                    elif res.type == MoveResult.Type.CAPTURE:
+                        actions.append(Action("move_capture", *res.pos))
+                        break
 
         return actions
