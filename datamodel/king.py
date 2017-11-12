08c688e3f263fe1fe08a6816bf34aa664d177a8e
mynusplus <33581931+mynusplus@users.noreply.github.com>
2017-11-12 01:57:22 -0600

    Update king.py

--- datamodel/king.py
+++ datamodel/king.py
@@ -5,12 +5,13 @@ class King(Pawn):
 	def get_actions(self):
 		actions = []
 
-		for vector in ((0, 1, 0), (1, 0, 0), (1, 1, 0), (0, -1, 0), (-1, 0, 0),
+		if can_move == True:
+			for vector in ((0, 1, 0), (1, 0, 0), (1, 1, 0), (0, -1, 0), (-1, 0, 0),
 					   (-1, -1, 0), (1, -1, 0), (-1, 1, 0)):
-			res = self.game.step_move_to(self, self.pos, vector)
-			if res.type == MoveResult.Type.REGULAR:
-				actions.append(Action("move", *res.pos))
-			elif res.type == MoveResult.Type.CAPTURE:
-				actions.append(Action("move_capture", *res.pos))
+				res = self.game.step_move_to(self, self.pos, vector)
+				if res.type == MoveResult.Type.REGULAR:
+					actions.append(Action("move", *res.pos))
+				elif res.type == MoveResult.Type.CAPTURE:
+					actions.append(Action("move_capture", *res.pos))
 
 		return actions
