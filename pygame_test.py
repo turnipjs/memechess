e8e898f1b0f2a4e70dc8af8f9bffdf11605b5b97
Baker8 <how2roof@gmail.com>
2017-11-12 09:46:58 -0600

    More coords added

--- pygame_test.py
+++ pygame_test.py
@@ -6,33 +6,78 @@ SQUARE_SIZE=50
 
 pygame.init()
 screen = pygame.display.set_mode((20*SQUARE_SIZE, 20*SQUARE_SIZE))
-ds
-king_spots = (19, 0, 0)
-queen_spot = (17, 1, 0)
 
 def turnwise_symmetry(original):
-	original = (19-original(1), 19-original(0), 0)
-	return original
+	base = (19-original[0], 19-original[1], 0)
+	return base
+
+""" LEGIT ACTUAL COORDS NOT THE BOOTLEGGED ONES WE DID FOR THE EXPO
+king_coords = (0, 19, 0)
+queen_coords = (1, 17, 0)
+pope_coords = (2, 19, 0)
+
+
+pawn_coords = [(2, 15, 0), (2, 14, 0), (2, 13, 0), (2, 12, 0), (2, 11, 0),
+			   (4, 17, 0), (5, 17, 0), (6, 17, 0), (7, 17, 0), (8, 17, 0)]
+bishop_coords = [(0, 15, 0), (1, 15, 0), (4, 19, 0), (4, 18, 0)]
+knight_coords = [(10, 18, 0), (1, 10, 0)]
+rook_coords = [(16, 0, 0), (19, 3, 0)]
+beekeeper_coords = []
+bowman_coords = []
+mage_coords = []
+
+"""
+
+king_coords = (0, 19, 0)
+queen_coords = ()
 
 board = game.Game()
 
 pieces_and_locations = []
 
-'''
+#pieces_and_locations.append(king.King(board, king_coords, game.Color.WHITE))
+pieces_and_locations.append(king.King(board, turnwise_symmetry(king_coords), game.Color.BLACK))
+
+pieces_and_locations.append(queen.Queen(board, queen_coords, game.Color.WHITE))
+pieces_and_locations.append(queen.Queen(board, turnwise_symmetry(queen_coords), game.Color.BLACK))
+
+pieces_and_locations.append(pope.Pope(board, pope_coords, game.Color.WHITE))
+pieces_and_locations.append(pope.Pope(board, turnwise_symmetry(pope_coords), game.Color.BLACK))
+
+for i in pawn_coords:
+	pieces_and_locations.append(pawn.Pawn(board, i, game.Color.WHITE))
+	pieces_and_locations.append(pawn.Pawn(board, turnwise_symmetry(i), game.Color.BLACK))
+
+for i in bishop_coords:
+	pieces_and_locations.append(bishop.Bishop(board, i, game.Color.WHITE))
+	pieces_and_locations.append(bishop.Bishop(board, turnwise_symmetry(i), game.Color.BLACK))
+
+for i in knight_coords:
+	pieces_and_locations.append(knight.Knight(board, i, game.Color.WHITE))
+	pieces_and_locations.append(knight.Knight(board, turnwise_symmetry(i), game.Color.BLACK))
+
+for i in rook_coords:
+	pieces_and_locations.append(rook.Rook(board, turnwise_symmetry(i), game.Color.WHITE))
+	pieces_and_locations.append(rook.Rook(board, i, game.Color.BLACK))
+
+
+
+#pieces_and_locations.append()
+
 for each_piece in pieces_and_locations:
-	board.add_piece(board)
-	piece = piece 
-'''
+	board.add_piece(each_piece)
 
-a=highrise.Highrise(board, (5, 5, 0), game.Color.WHITE)
+a=king.King(board, king_coords, game.Color.WHITE)
 board.add_piece(a)
-b=highrise.Highrise(board, (6, 3, 0), game.Color.WHITE)
-board.add_piece(b)
 
 
 
-[board.add_piece(i) for i in portal.make_cave_pair(board, (13, 13, 0), (2, 2, 0))]
-[board.add_piece(i) for i in portal.make_cave_pair(board, (13, 13, 1), (2, 2, 1))]
+for i in range(0, 7):
+	[board.add_piece(i) for i in portal.make_cave_pair(board, (9, 6, i), (9, 13, i))]
+	[board.add_piece(i) for i in portal.make_cave_pair(board, (10, 6, i), (10, 13, i))]
+	[board.add_piece(i) for i in portal.make_cave_pair(board, (6, 10, i), (13, 10, i))]
+	[board.add_piece(i) for i in portal.make_cave_pair(board, (6, 9, i), (13, 9, i))]
+
 
 selected_piece = a
 
