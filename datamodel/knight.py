5577be1c0b7e0293b83e7b2e6b713efde20f24f7
mynusplus <33581931+mynusplus@users.noreply.github.com>
2017-11-12 01:58:30 -0600

    Update knight.py

--- datamodel/knight.py
+++ datamodel/knight.py
@@ -5,30 +5,32 @@ from .game import Action, MoveResult
 class Knight(Pawn):
     def get_actions(self):
         actions = []
-        x = (1, 0, 0)
-        y = (0, 1, 0)
-        nx = (-1, 0, 0)
-        ny = (0, -1, 0)
-        horse_movements = [[x, y, y], [y, y, x], [x, x, y], [y, x, x], [x, x, ny], [ny, x, x], [x, ny, ny], [ny, ny, x], [nx, ny, ny], [ny, ny, nx], [nx, nx, ny], [ny, nx, nx], [nx, nx, y], [y, nx, nx], [nx, y, y], [y, nx, nx]]
-        for move in horse_movements:
-            res = MoveResult(MoveResult.Type.REGULAR, self.pos)
-            res = self.game.step_move_to(self, res.pos, (0, 0, 1))
-            up_to_3 = 0
-            for step in move:
-                res = self.game.step_move_to(self, res.pos, step)
-                if res.type == MoveResult.Type.INVALID:
-                    break
-                elif res.type == MoveResult.Type.REGULAR:
-                    up_to_3 += 1
-                    if res.ends_motion:
+        
+        if can_move == True:
+            x = (1, 0, 0)
+            y = (0, 1, 0)
+            nx = (-1, 0, 0)
+            ny = (0, -1, 0)
+            horse_movements = [[x, y, y], [y, y, x], [x, x, y], [y, x, x], [x, x, ny], [ny, x, x], [x, ny, ny], [ny, ny, x], [nx, ny, ny], [ny, ny, nx], [nx, nx, ny], [ny, nx, nx], [nx, nx, y], [y, nx, nx], [nx, y, y], [y, nx, nx]]
+            for move in horse_movements:
+                res = MoveResult(MoveResult.Type.REGULAR, self.pos)
+                res = self.game.step_move_to(self, res.pos, (0, 0, 1))
+                up_to_3 = 0
+                for step in move:
+                    res = self.game.step_move_to(self, res.pos, step)
+                    if res.type == MoveResult.Type.INVALID:
                         break
+                    elif res.type == MoveResult.Type.REGULAR:
+                        up_to_3 += 1
+                        if res.ends_motion:
+                            break
 
-            res = self.game.step_move_to(self, res.pos, (0, 0, -1))
-            if up_to_3 == 3:
-                if res.type == MoveResult.Type.REGULAR and Action("move", *res.pos) not in actions:
-                    actions.append(Action("move", *res.pos))
-                elif res.type == MoveResult.Type.CAPTURE and Action("move_capture", *res.pos) not in actions:
-                    actions.append(Action("move_capture", *res.pos))
+                res = self.game.step_move_to(self, res.pos, (0, 0, -1))
+                if up_to_3 == 3:
+                    if res.type == MoveResult.Type.REGULAR and Action("move", *res.pos) not in actions:
+                        actions.append(Action("move", *res.pos))
+                    elif res.type == MoveResult.Type.CAPTURE and Action("move_capture", *res.pos) not in actions:
+                        actions.append(Action("move_capture", *res.pos))
 
         return actions
 
