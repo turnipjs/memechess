8d3888919750498c659c22b7e8ae7a966129ba7e
mynusplus <33581931+mynusplus@users.noreply.github.com>
2017-11-12 01:56:31 -0600

    Update bowman.py

--- datamodel/bowman.py
+++ datamodel/bowman.py
@@ -5,16 +5,17 @@ class Bowman(Piece):
     def get_actions(self):
         actions = []
         
-        for vector in ((0, 1, 0), (1, 0, 0), (1, 1, 0), (0, -1, 0), (-1, 0, 0),
-                       (-1, -1, 0), (1, -1, 0), (-1, 1, 0)):
-            res = self.game.step_move_to(self, self.pos, vector)
-            if res.type == MoveResult.Type.REGULAR:
-                actions.append(Action("move", *res.pos))
+        if can_move == True:
+            for vector in ((0, 1, 0), (1, 0, 0), (1, 1, 0), (0, -1, 0), (-1, 0, 0),
+                        (-1, -1, 0), (1, -1, 0), (-1, 1, 0)):
+                res = self.game.step_move_to(self, self.pos, vector)
+                if res.type == MoveResult.Type.REGULAR:
+                    actions.append(Action("move", *res.pos))
 
-        for vector in ((0, 2, 0), (2, 0, 0), (2, 2, 0), (0, -2, 0), (-2, 0, 0), (-2, -2, 0), (-2, 2, 0), (2, -2, 0), (1, 2, 0), (2, 1, 0), (-1, 2, 0), (2, -1, 0), (1, -2, 0), (-2, 1, 0), (-1, -2, 0), (-2, -1, 0)):
-            res = self.game.step_move_to(self, self.pos, vector)
-            if res.type == MoveResult.Type.CAPTURE:
-                actions.append(Action("capture", *res.pos))
+            for vector in ((0, 2, 0), (2, 0, 0), (2, 2, 0), (0, -2, 0), (-2, 0, 0), (-2, -2, 0), (-2, 2, 0), (2, -2, 0), (1, 2, 0), (2, 1, 0), (-1, 2, 0), (2, -1, 0), (1, -2, 0), (-2, 1, 0), (-1, -2, 0), (-2, -1, 0)):
+                res = self.game.step_move_to(self, self.pos, vector)
+                if res.type == MoveResult.Type.CAPTURE:
+                    actions.append(Action("capture", *res.pos))
 
         return actions
 
